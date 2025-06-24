e0bc46b2bfaf3c2fe8c42a20bb88d64ed6276727
thebowlingeur@hotmail.com <tyckyz-bYshob-cepbo4>
2025-06-24 14:51:40 +0200

    Correction configuration for comwatt_indepbox component

--- custom_components/comwatt_indepbox/config_flow.py
+++ custom_components/comwatt_indepbox/config_flow.py
@@ -4,13 +4,11 @@ from homeassistant.core import callback
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
+from .const import DOMAIN
 from .client import ComwattClient
 
 @config_entries.HANDLERS.register(DOMAIN)
 class ComwattConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Gère le flux de configuration pour Comwatt Indepbox."""
-
     VERSION = 1
 
     def __init__(self):
@@ -26,56 +24,18 @@ class ComwattConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             try:
                 client = ComwattClient(username, password)
                 await client.authenticate()
-                self.sites = await client.get_sites()
-                self.devices = []
-                for site in self.sites:
-                    self.devices.extend(await client.get_devices(site["id"]))
-
-                # Sauvegarde temporaire dans context
-                self.context_data = {
-                    "username": username,
-                    "password": password,
-                    "devices": self.devices
-                }
-
-                return await self.async_step_device_selection()
             except Exception:
                 self._errors["base"] = "invalid_auth"
+            else:
+                return self.async_create_entry(title="Comwatt Indepbox", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_USERNAME): cv.string,
-                    vol.Required(CONF_PASSWORD): cv.string,
-                }
-            ),
-            errors=self._errors,
-        )
-
-    async def async_step_device_selection(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(
-                title="Comwatt Indepbox",
-                data={
-                    CONF_USERNAME: self.context_data["username"],
-                    CONF_PASSWORD: self.context_data["password"],
-                    "production_autoconso": user_input["production_autoconso"],
-                    "production_revente": user_input["production_revente"],
-                    "consommation": user_input["consommation"]
-                }
-            )
-
-        device_names = {str(d["id"]): d["name"] for d in self.context_data["devices"]}
-
-        return self.async_show_form(
-            step_id="device_selection",
             data_schema=vol.Schema({
-                vol.Required("production_autoconso"): vol.In(device_names),
-                vol.Required("production_revente"): vol.In(device_names),
-                vol.Required("consommation"): vol.In(device_names),
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
             }),
-            description_placeholders=device_names
+            errors=self._errors,
         )
 
     @staticmethod
@@ -83,36 +43,36 @@ class ComwattConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(config_entry):
         return OptionsFlowHandler(config_entry)
 
+
 class OptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
-        self.client = ComwattClient(
-            config_entry.data[CONF_USERNAME],
-            config_entry.data[CONF_PASSWORD]
-        )
-        self.devices = []
+        self.device_options = {}
 
     async def async_step_init(self, user_input=None):
-        # Authentifie et récupère les devices si ce n’est pas déjà fait
-        await self.client.authenticate()
-        self.devices = await self.client.get_devices()
+        hass = self.hass
+        username = self.config_entry.data[CONF_USERNAME]
+        password = self.config_entry.data[CONF_PASSWORD]
 
-        # Construit une map id → nom
-        device_choices = {str(d["id"]): d["name"] for d in self.devices}
+        try:
+            client = ComwattClient(username, password)
+            await client.authenticate()
+            devices = await client.get_devices()
+            self.device_options = {
+                str(device["id"]): device["name"]
+                for device in devices if "name" in device
+            }
+        except Exception:
+            return self.async_abort(reason="cannot_connect")
 
-        options = self.config_entry.options
-        default_consumption = options.get("consumption", [])
-        default_self = options.get("production_autoconsommation", [])
-        default_export = options.get("production_revente", [])
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Optional("consumption", default=default_consumption): cv.multi_select(device_choices),
-                vol.Optional("production_autoconsommation", default=default_self): cv.multi_select(device_choices),
-                vol.Optional("production_revente", default=default_export): cv.multi_select(device_choices),
-            }),
+                vol.Optional("consumption", default=self.config_entry.options.get("consumption", [])): cv.multi_select(self.device_options),
+                vol.Optional("production_autoconsommation", default=self.config_entry.options.get("production_autoconsommation", [])): cv.multi_select(self.device_options),
+                vol.Optional("production_revente", default=self.config_entry.options.get("production_revente", [])): cv.multi_select(self.device_options),
+            })
         )
-
-    async def async_step_user(self, user_input=None):
-        return self.async_create_entry(title="", data=user_input)
